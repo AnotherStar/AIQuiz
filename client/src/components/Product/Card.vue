d02f3aece21bf82ff21f9347fa18dfdd83ad1b12
Dmitriy <AnotherStar@users.noreply.github.com>
2024-09-26 21:46:22 +0300

    Initial commit

---
+++ client/src/components/Product/Card.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <pre>{{ product }}</pre>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref, PropType, watch, computed } from 'vue';
+import { Product } from '@server/product/products.database';
+
+const props = defineProps({
+    product: {
+        type: Object as PropType<Product>,
+        required: true,
+    },
+});
+</script>
