4ba35c8f08fe6f147def24ac0186dc5b76438a6e
Dmitriy Kuznetcov <dmitriy_kuznetcov@protonmail.com>
2024-11-21 23:54:30 +0300

    ...

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,28 @@
 const apps = [
     {
-        name: 'Nest backend',
+        name: 'Nest backend quiz',
         kill_timeout: 5000,
         wait_ready: true,
-        script: 'cd server && dist/main.js',
+        script: 'cd server && node dist/main.js',
         autorestart: true,
-        node_args: ['--color'],
         time: true,
         log_date_format: 'YYYY-MM-DD HH:mm:ss.SSS Z',
     },
 ];
 
+const deploy = {
+    production: {
+        user: 'root',
+        host: 'quiz.another-star.ru',
+        ref: 'origin/main',
+        repo: 'git@github.com:AnotherStar/AIQuiz.git',
+        path: '/var/www/quiz.another-star.ru',
+        'post-deploy':
+            'node -v && cd client && npm install && npm run build && cd ../server && npm install && npm run build && cd ../ && pm2 reload ecosystem.config.js --env production',
+    },
+};
+
 module.exports = {
     apps,
+    deploy,
 };
