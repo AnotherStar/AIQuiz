a1031cfa146f2d37ec695083219753e4f9e2c213
Dmitriy Kuznetcov <dmitriy_kuznetcov@protonmail.com>
2024-11-22 14:55:59 +0300

    ...

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="bg-pink-800 w-full h-screen fixed overflow-y-auto">
+    <div
+        class="bg-pink-800 w-full h-screen fixed overflow-y-auto overflow-x-hidden"
+        style="max-width: 100vw"
+    >
         <RouterView />
     </div>
 </template>
